e0e7bcf78c5467a065e144c87138e5c51f944fd1
August Johnson <gurojohnson@gmail.com>
2026-06-15 15:59:02 -0400

    Feat: write HomePage.jsx to display home page content also link home page to the shop page in addition to the nav bar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
-
+import { Link } from "react-router-dom";
+import '../styles/home-page.css';
 
 function HomePage() {
     return (
+        // let's throw something generic content in here to fill the void
         <div>
-            <p>This is the home page</p>
+            <h2>Welcome to the Home Page of my store!</h2>
+            <p>This isn't a real store just yet but has definitely been some good practice with React!</p>
+            <p>Check out my Github and other socials below!</p>
+            <Link to='/shop'>Get Shopping</Link>
+
+        {
+        //some type of footer with links to Github etc. will go here 
+        }
+        <p>** links to socials will go here **</p>
+
         </div>
     )
 }
