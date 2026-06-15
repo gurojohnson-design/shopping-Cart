29f44605e83c55543681cf14c4fa9c6b713ec2ef
August Johnson <gurojohnson@gmail.com>
2026-06-15 16:59:42 -0400

    Feat: Feat: write fetch request for pulling products from API and display list of items

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,8 +1,38 @@
-
+import { useEffect, useState } from "react";
 
 function ShopPage() {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then((response) => {
+            if (response.status >= 400) {
+                throw new Error('server error');
+            }
+            return response.json();
+        })
+        .then((response) => setProducts(response))
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>A network error was encountered</p>;
+
+
+
     return (
         <div>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>{product.title}</li>
+                ))}
+            </ul>
+
+
+
             <p>This is the shop page</p>
         </div>
     )
