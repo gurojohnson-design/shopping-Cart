764bae93f01bcb5d45ebacc89acdffb961f08117
August Johnson <gurojohnson@gmail.com>
2026-06-18 13:36:26 -0400

    Feat: write CartPage and display an interactive cart using CartItem

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,32 @@
+import { useOutletContext, Link } from "react-router-dom";
+import CartItem from "../components/CartItem/CartItem";
+
+
+//updateQuantity, cart
 
 
 function CartPage() {
-    return (
-        <div>
-            <p>This is the cart page</p>
-        </div>
-    )
+    const { cart, updateQuantity} = useOutletContext();
+
+
+    if(cart.length === 0) {
+        return (
+            <>
+            <h2>Nothing to see here.</h2>
+            <p><Link to='/shop' >Get shopping!</Link></p>
+            </>
+        )
+    } else {
+        return (
+            <div>
+                <ul>
+                    {cart.map((product) => (
+                        <CartItem key={product.id} item={product} updateQuantity={updateQuantity} />
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default CartPage;
